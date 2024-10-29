1f24acbf6af039981c22828e463d3ce65a9fbd10
AndreyHernandezRodriguez55 <186082071+AndreyHernandezRodriguez55@users.noreply.github.com>
2024-10-29 12:01:39 -0500

    modificados:     main3.py

---
+++ main3.py
@@ -0,0 +1,6 @@
+fullName= input("Enter your full name:")
+age= int(input("Enter your age:"))
+gender = input("enter your possible gender option:M,F,X")
+heigth=  float(input("Enter your height"))
+
+print("""hola,usuario {fullName}. Como estas? te saluda python.gracias compartir tu edad {age}; no sabia que eras un humano de tipo  {gender}""")
