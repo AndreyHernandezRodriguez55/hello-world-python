4966d9bbcad5988a8f0ad68a198c300875a88fb9
AndreyHernandezRodriguez55 <186082071+AndreyHernandezRodriguez55@users.noreply.github.com>
2024-10-29 11:19:39 -0500

    tipos de datos en python

---
+++ main2.py
@@ -0,0 +1,13 @@
+fullname = "Andrey Hernandez Rodriguez" #tipo de dato string
+geneder = "M" #tipo de dato char
+age = 19 #tipo de dato int
+height = 1,70 #tipo de doble
+trainer = False #tipo de dato boolean
+
+# conjuntos:
+hobbies = ["mma","powerlifting",'LOL Nautilus player'] #tipo de dato array
+Address = (dict)["country":"Colombia","state":"santander"] #tipo de datos object
+
+print(f"""studen name: "{fullname}
+stiden age: {age}
+""")
